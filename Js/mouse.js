cd90dc790d1a313ea1885572ae6942444877223e
Rafael <riolivaresv@gmail.com>
2020-03-17 23:30:24 -0700

    scroll moving added, also frame stay in the same place

--- Js/mouse.js
+++ Js/mouse.js
@@ -1,17 +1,30 @@
 var scrollTop = 0;
-var scrollDown = screen.width;
+var scrollDown = 0;
+var leftPosotion = 0;
+// console.log(scrollDown);
 window.addEventListener("wheel", function(event){
-    // console.log(event.deltaY);
+    scrollMoving(event);
+});
+
+function scrollMoving(event){
     if (event.deltaY>0) {
         if (scrollTop<scrollDown) {
             scrollTop+=10;   
+            leftPosotion += 10;
         }
     } else{
         if (scrollTop>0) {
             scrollTop-=10;   
+            leftPosotion -= 10;
         }
     }
-    console.log(scrollTop);
+    //  console.log(scrollTop);
     window.scrollTo(scrollTop,0);
-});
+    document.getElementById("containerPresentation").style.left = leftPosotion + "px";
+}
 
+
+window.onload = function(){ 
+    scrollDown = $(document).width();
+    this.leftPosotion = document.getElementById("containerPresentation").offsetLeft;
+};
